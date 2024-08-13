9ac526c05210fb950af01c413e7396a74bbdb2b5
gokulgovinthan <goodgovinthan@gmail.com>
2024-08-13 08:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function shortcut(s1, s2) {
-  // your code here
+  if(typeof s1== "string" && typeof s2== "string"){
+	  let shortcut= s1.charAt(0)+ s2.charAt(0);
+	   return shortcut;
+  }
+	else{
+
+	  return "";
+	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
